c330258c280660a73a5f8ca12d8c303266b406cd
Danny <dannyuk@live.co.uk>
2024-07-12 12:52:58 +0100

    JobIDs added
    Could have been an enum as I don't expect square to add a billion jobs, but that would require casting all the time and thats annoying.
    
    Just a easy location to get the job IDs from

---
+++ XIVComboPlugin/JobIDs.cs
@@ -0,0 +1,26 @@
+﻿namespace XIVComboPlugin;
+
+public static class JobIDs
+{
+    public const uint AST = 33;
+    public const uint BLM = 25;
+    public const uint BRD = 23;
+    public const uint DNC = 38;
+    public const uint DRG = 22;
+    public const uint DRK = 32;
+    public const uint GNB = 37;
+    public const uint MCH = 31;
+    public const uint MNK = 20;
+    public const uint NIN = 30;
+    public const uint PCT = 42;
+    public const uint PLD = 19;
+    public const uint RDM = 35;
+    public const uint RPR = 39;
+    public const uint SAM = 34;
+    public const uint SCH = 28;
+    public const uint SGE = 40;
+    public const uint SMN = 27;
+    public const uint VPR = 41;
+    public const uint WAR = 21;
+    public const uint WHM = 24;
+}
