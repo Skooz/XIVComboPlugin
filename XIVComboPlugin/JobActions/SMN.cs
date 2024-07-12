59f12fb39c330a27359610ac0510ab0a9d27ba8c
Danny <dannyuk@live.co.uk>
2024-07-12 12:52:16 +0100

    SMN abstraction
    Including a fix for Fester/Necrotize as it wouldn't work when synced below 92

--- XIVComboPlugin/JobActions/SMN.cs
+++ XIVComboPlugin/JobActions/SMN.cs
@@ -1,7 +1,14 @@
-﻿namespace XIVComboPlugin.JobActions
+﻿using Dalamud.Game.ClientState.JobGauge.Types;
+using Dalamud.Hooking;
+using Dalamud.Plugin.Services;
+
+namespace XIVComboPlugin.JobActions
 {
-    public static class SMN
+    public class SMN : JobBase
     {
+        public override string JobDisplayName => "Summoner";
+        public override uint JobID => JobIDs.SMN;
+
         public const uint
             Deathflare = 3582,
             EnkindlePhoenix = 16516,
@@ -19,5 +26,47 @@
             EnergyDrain = 16508,
             Painflare = 3578,
             EnergySyphon = 16510;
+
+        private SMNGauge Gauge;
+        
+        public SMN(IClientState state, XIVComboConfiguration config, IJobGauges gauges, IPluginLog log) : base(state, config, gauges, log)
+        {
+            Gauge = gauges.Get<SMNGauge>();
+        }
+
+        public override ulong IconDetour(Hook<IconReplacer.OnGetIconDelegate> iconHook, byte self, uint actionID)
+        {
+            if (clientState.LocalPlayer == null) return iconHook.Original(self, actionID);
+            
+            // Change Fester into Energy Drain
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.SummonerEDFesterCombo))
+            {
+                if (actionID == Fester || (actionID == Necrotize && level >= 92))
+                {
+                    if (!Gauge.HasAetherflowStacks)
+                    {
+                        return EnergyDrain;
+                    }
+                    
+                    return actionID;
+                }
+            }
+
+            //Change Painflare into Energy Syphon
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.SummonerESPainflareCombo))
+            {
+                if (actionID == Painflare)
+                {
+                    if (!Gauge.HasAetherflowStacks)
+                    {
+                        return EnergySyphon;
+                    }
+                    
+                    return Painflare;
+                }
+            }
+            
+            return iconHook.Original(self, actionID);
+        }
     }
 }
