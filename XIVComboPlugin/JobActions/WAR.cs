dee99570e4ae159e9e1df8f17ade9125079faf27
Danny <dannyuk@live.co.uk>
2024-07-12 12:52:23 +0100

    WAR/WHM abstractions

--- XIVComboPlugin/JobActions/WAR.cs
+++ XIVComboPlugin/JobActions/WAR.cs
@@ -1,7 +1,14 @@
-﻿namespace XIVComboPlugin.JobActions
+﻿using Dalamud.Game.ClientState.JobGauge.Types;
+using Dalamud.Hooking;
+using Dalamud.Plugin.Services;
+
+namespace XIVComboPlugin.JobActions
 {
-    public static class WAR
+    public class WAR : JobBase
     {
+        public override string JobDisplayName => "Warrior";
+        public override uint JobID => JobIDs.WAR;
+
         public const uint
             StormsPath = 42,
             HeavySwing = 31,
@@ -18,5 +25,95 @@
             BuffPrimalRendReady = 2624,
             BuffWrathful = 3901,
             BuffPrimalRuinationReady = 3834;
+
+        public WAR(IClientState state, XIVComboConfiguration config, IJobGauges gauges, IPluginLog log) : base(state, config, gauges, log)
+        {
+        }
+
+        public override ulong IconDetour(Hook<IconReplacer.OnGetIconDelegate> iconHook, byte self, uint actionID)
+        {
+            if (clientState.LocalPlayer == null) return iconHook.Original(self, actionID);
+
+            // Replace Storm's Path with Storm's Path combo
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WarriorStormsPathCombo))
+            {
+                if (actionID == StormsPath)
+                {
+                    if (comboTime > 0)
+                    {
+                        if (lastMove == HeavySwing && level >= 4)
+                        {
+                            return Maim;
+                        }
+                        
+                        if (lastMove == Maim && level >= 26)
+                        {
+                            return StormsPath;
+                        }
+                    }
+
+                    return HeavySwing;
+                }
+            }
+
+            // Replace Storm's Eye with Storm's Eye combo
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WarriorStormsEyeCombo))
+            {
+                if (actionID == StormsEye)
+                {
+                    if (comboTime > 0)
+                    {
+                        if (lastMove == HeavySwing && level >= 4)
+                        {
+                            return Maim;
+                        }
+                        
+                        if (lastMove == Maim && level >= 50)
+                        {
+                            return StormsEye;
+                        }
+                    }
+
+                    return HeavySwing;
+                }
+            }
+
+            // Replace Mythril Tempest with Mythril Tempest combo
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WarriorMythrilTempestCombo))
+            {
+                if (actionID == MythrilTempest)
+                {
+                    if (comboTime > 0)
+                    {
+                        if (lastMove == Overpower && level >= 40)
+                        {
+                            return MythrilTempest;
+                        }
+                    }
+                    
+                    return Overpower;
+                }
+            }
+
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WarriorIRCombo))
+            {
+                if (actionID is InnerRelease or Berserk)
+                {
+                    if (!HasBuff(BuffWrathful) && HasBuff(BuffPrimalRendReady))
+                    {
+                        return PrimalRend;
+                    }
+                    
+                    if (!HasBuff(BuffWrathful) && HasBuff(BuffPrimalRuinationReady))
+                    {
+                        return PrimalRuination;
+                    }
+                    
+                    return iconHook.Original(self, actionID);
+                }
+            }
+
+            return iconHook.Original(self, actionID);
+        }
     }
 }
--- XIVComboPlugin/JobActions/WHM.cs
+++ XIVComboPlugin/JobActions/WHM.cs
@@ -1,12 +1,74 @@
-﻿namespace XIVComboPlugin.JobActions
+﻿using Dalamud.Game.ClientState.JobGauge.Types;
+using Dalamud.Hooking;
+using Dalamud.Plugin.Services;
+
+namespace XIVComboPlugin.JobActions
 {
-	public static class WHM
-	{
-		public const uint
-			Cure = 120,
-			Cure2 = 135,
-			Solace = 16531,
-			Rapture = 16534,
-			Misery = 16535;
-	}
+    public class WHM : JobBase
+    {
+        public override string JobDisplayName => "White Mage";
+        public override uint JobID => JobIDs.WHM;
+
+        public const uint
+            Cure = 120,
+            Cure2 = 135,
+            Solace = 16531,
+            Rapture = 16534,
+            Misery = 16535;
+
+        private WHMGauge Gauge;
+
+        public WHM(IClientState state, XIVComboConfiguration config, IJobGauges gauges, IPluginLog log) : base(state, config, gauges, log)
+        {
+            Gauge = gauges.Get<WHMGauge>();
+        }
+
+        public override ulong IconDetour(Hook<IconReplacer.OnGetIconDelegate> iconHook, byte self, uint actionID)
+        {
+            if (clientState.LocalPlayer == null) return iconHook.Original(self, actionID);
+
+            // Replace Solace with Misery when full blood lily
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WhiteMageSolaceMiseryFeature))
+            {
+                if (actionID == Solace)
+                {
+                    if (Gauge.BloodLily == 3)
+                    {
+                        return Misery;
+                    }
+
+                    return Solace;
+                }
+            }
+
+            // Replace Solace with Misery when full blood lily
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WhiteMageRaptureMiseryFeature))
+            {
+                if (actionID == Rapture)
+                {
+                    if (Gauge.BloodLily == 3)
+                    {
+                        return Misery;
+                    }
+
+                    return Rapture;
+                }
+            }
+            
+            if (Configuration.ComboPresets.HasFlag(CustomComboPreset.WhiteMageCureSync))
+            {
+                if (actionID == Cure2)
+                {
+                    if (level < 30)
+                    {
+                        return Cure;
+                    }
+                    
+                    return Cure2;
+                }
+            }
+
+            return iconHook.Original(self, actionID);
+        }
+    }
 }
